008656611ce8e0aa60b8c4b194f88c53b5e7674f
Yuriy Martini <y.martini@soluzionesoftware.com>
2021-03-30 09:44:32 +0200

    refactoring/styling

--- src/Translatable.php
+++ src/Translatable.php
@@ -2,8 +2,11 @@
 
 namespace SoluzioneSoftware\Nova\Fields;
 
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -16,7 +19,10 @@ class Translatable extends Field
      */
     public $component = 'translatable-field';
 
-    private $locales = [];
+    /**
+     * @var array
+     */
+    private $locales;
 
     /** @var Field */
     private $field;
@@ -27,17 +33,21 @@ class Translatable extends Field
     /**
      * Create a new field.
      *
-     * @param Field $field
+     * @param  Field  $field
      */
     public function __construct(Field $field)
     {
         parent::__construct($field->name, $field->attribute, $field->resolveCallback);
 
-        $locales = app('translatable.locales')->all();
-        $this->locales = array_combine($locales, array_map(function ($value){return strtoupper($value);}, $locales));
+        $locales = App::make('translatable.locales')->all();
+        $this->locales = array_combine($locales, array_map(function ($value) {
+            return strtoupper($value);
+        }, $locales));
         $this->field = $field;
 
-        $fields = array_map(function ($locale) use ($field) {return $this->localizeField(clone $field, $locale);}, $locales);
+        $fields = array_map(function ($locale) use ($field) {
+            return $this->localizeField(clone $field, $locale);
+        }, $locales);
         $this->fields = array_combine($locales, $fields);
 
         $this->withMeta([
@@ -46,45 +56,12 @@ class Translatable extends Field
             'originalField' => $this->field,
         ]);
 
-        $this->indexLocale(app()->getLocale());
+        $this->indexLocale(App::getLocale());
 
         $this->showOnIndex = $this->field->showOnIndex;
         $this->showOnDetail = $this->field->showOnDetail;
         $this->showOnCreation = $this->field->showOnCreation;
         $this->showOnUpdate = $this->field->showOnUpdate;
-    }
-
-    public function indexLocale($locale)
-    {
-        return $this->withMeta(['indexLocale' => $locale]);
-    }
-
-    /**
-     * @param \Astrotomic\Translatable\Contracts\Translatable $resource
-     * @param string|null $attribute
-     * @return void
-     */
-    public function resolve($resource, $attribute = null)
-    {
-        /** @var Field $field */
-        foreach ($this->fields as $localeCode => $field) {
-            $resource->setDefaultLocale($localeCode);
-            $field->resolve($resource, $this->field->attribute);
-        }
-
-        return;
-    }
-
-    /**
-     * Resolve the given attribute from the given resource.
-     *
-     * @param  mixed  $resource
-     * @param  string  $attribute
-     * @return mixed
-     */
-    protected function resolveAttribute($resource, $attribute)
-    {
-        return $resource->translations->pluck($attribute, config('translatable.locale_key'));
     }
 
     protected function localizeField(Field $field, string $locale)
@@ -98,29 +75,22 @@ class Translatable extends Field
         return is_null($attribute) ? null : "translatable_{$locale}_{$attribute}";
     }
 
+    public function indexLocale($locale)
+    {
+        return $this->withMeta(['indexLocale' => $locale]);
+    }
+
     /**
-     * Hydrate the given attribute on the model based on the incoming request.
-     *
-     * @param NovaRequest $request
-     * @param  string  $requestAttribute
-     * @param  Model  $model
-     * @param  string  $attribute
+     * @param  TranslatableContract  $resource
+     * @param  string|null  $attribute
      * @return void
      */
-    protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
+    public function resolve($resource, $attribute = null)
     {
-        /** @var array $requestData */
-        $requestData = $request->all();
-
-        foreach ($this->locales as $localeCode => $locale) {
-            $value = $request->get($this->localizeAttribute($localeCode, $requestAttribute));
-
-            $requestData[$requestAttribute] = $value;
-            $request->replace($requestData);
-
-            $model->setDefaultLocale($localeCode);
-
-            $this->field->fillAttributeFromRequest($request, $requestAttribute, $model, $attribute);
+        /** @var Field $field */
+        foreach ($this->fields as $localeCode => $field) {
+            $resource->setDefaultLocale($localeCode);
+            $field->resolve($resource, $this->field->attribute);
         }
     }
 
@@ -141,7 +111,7 @@ class Translatable extends Field
             'panel' => $this->panel,
             'sortable' => $this->sortable,
             'nullable' => $this->nullable,
-            'readonly' => $this->isReadonly(app(NovaRequest::class)),
+            'readonly' => $this->isReadonly(App::make(NovaRequest::class)),
             'textAlign' => $this->textAlign,
         ], $this->meta());
     }
@@ -151,11 +121,11 @@ class Translatable extends Field
      * @return string
      * @throws Exception
      */
-    function __get($name){
-        if (isset($this->$name)){
+    function __get($name)
+    {
+        if (isset($this->$name)) {
             return $this->$name;
-        }
-        elseif (isset($this->field->$name)){
+        } elseif (isset($this->field->$name)) {
             return $this->field->$name;
         }
 
@@ -164,9 +134,48 @@ class Translatable extends Field
 
     public function __call($method, $arguments)
     {
-        if (method_exists($this->field, $method))
+        if (method_exists($this->field, $method)) {
             return call_user_func_array($this->field->$method, $arguments);
+        }
 
         return parent::__call($method, $arguments);
+    }
+
+    /**
+     * Resolve the given attribute from the given resource.
+     *
+     * @param  mixed  $resource
+     * @param  string  $attribute
+     * @return mixed
+     */
+    protected function resolveAttribute($resource, $attribute)
+    {
+        return $resource->translations->pluck($attribute, Config::get('translatable.locale_key'));
+    }
+
+    /**
+     * Hydrate the given attribute on the model based on the incoming request.
+     *
+     * @param  NovaRequest  $request
+     * @param  string  $requestAttribute
+     * @param  Model  $model
+     * @param  string  $attribute
+     * @return void
+     */
+    protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
+    {
+        $requestData = $request->all();
+
+        foreach ($this->locales as $localeCode => $locale) {
+            $value = $request->get($this->localizeAttribute($localeCode, $requestAttribute));
+
+            $requestData[$requestAttribute] = $value;
+            $request->replace($requestData);
+
+            /** @var Model|TranslatableContract $model */
+            $model->setDefaultLocale($localeCode);
+
+            $this->field->fillAttributeFromRequest($request, $requestAttribute, $model, $attribute);
+        }
     }
 }
